6fa1a76976ceebc9993f15ba9df9bb6fe0afe297
Yash <yashkantharia17@gmail.com>
2020-06-19 17:46:15 +0530

    CT SDK 3.8.0
    Added:
    >Splash Screen
    >Push Template (Custom Handling)

--- app/src/main/java/com/example/clevertapper/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/clevertapper/MyFirebaseMessagingService.java
@@ -1,31 +1,53 @@
 package com.example.clevertapper;
 
+import android.annotation.SuppressLint;
 import android.app.Service;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import com.clevertap.android.sdk.CleverTapAPI;
 import com.clevertap.android.sdk.NotificationInfo;
+import com.clevertap.pushtemplates.Constants;
+import com.clevertap.pushtemplates.TemplateRenderer;
+import com.clevertap.pushtemplates.Utils;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import java.util.Map;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    Context context;
+    CleverTapAPI instance;
+
+    @SuppressLint("LongLogTag")
     @Override
-    public void onMessageReceived(RemoteMessage message){
+    public void onMessageReceived(RemoteMessage remoteMessage) {
         try {
-            if (message.getData().size() > 0) {
+            context = getApplicationContext();
+            if (remoteMessage.getData().size() > 0) {
                 Bundle extras = new Bundle();
-                for (Map.Entry<String, String> entry : message.getData().entrySet()) {
+                for (Map.Entry<String, String> entry : remoteMessage.getData().entrySet()) {
                     extras.putString(entry.getKey(), entry.getValue());
                 }
-                Log.e("TAG","onReceived Mesaage Called");
-                NotificationInfo info = CleverTapAPI.getNotificationInfo(extras);
-                if (info.fromCleverTap) {
-                    CleverTapAPI.createNotification(getApplicationContext(), extras);
+
+                instance = CleverTapAPI.getDefaultInstance(getApplicationContext());
+
+                boolean processCleverTapPN = Utils.isPNFromCleverTap(extras);
+
+                if (processCleverTapPN) {
+                    String pt_id = extras.getString(Constants.PT_ID);
+                    if (pt_id == null || pt_id.isEmpty()) {
+                        CleverTapAPI.createNotification(context, extras);
+                    } else {
+                        TemplateRenderer.setDebugLevel(2); //-1 for OFF, 0, for INFO, 2 for DEBUG, 3 for VERBOSE (errors)
+                        TemplateRenderer.createNotification(context, extras);
+                    }
+                } else {
+                    //Other providers
                 }
             }
-        } catch (Throwable t) {
-            Log.d("MYFCMLIST", "Error parsing FCM message", t);
+        } catch (Throwable throwable) {
+            Log.d("Error parsing FCM payload", String.valueOf(throwable));
         }
     }
     @Override
